a1e3724d472d7ec2cb44ccd6c8bce333e36f1ff1
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-18 16:38:57 +0100

    [UPD] Add sconto 20% per minorenni

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 // variabili
 
-let età = prompt('inserisci la tua età')
-let km = prompt('inserisci i km')
+let età = prompt('inserisci la tua età');
+let km = prompt('inserisci i km');
 
-let prezzo = 0.21 * km
+let prezzo = km * 0.21;
+let messaggio;
+
+
+// sconti
+if(età < 18){
+    prezzo = prezzo * 0.8;
+    messaggio = `hai diritto al 20% di sconto. Il prezzo del tuo biglietto è ${prezzo} euro`;
+}
+
+
+console.log(messaggio)
